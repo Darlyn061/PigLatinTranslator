0c4c8ea7691c2e01f9c86e1d14b3bc74f503609c
Darlyn061 <davaleriorosario@gmail.com>
2022-10-08 00:49:15 -0400

    Initial commit

---
+++ PigLatinTranslator/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+string PigLatinTranslator(string[] args)
+{
+
+    string[] words = args[0].Split(" ");
+    string pigLatin = "";
+    foreach (string word in words)
+    {
+        string pigLatinWord = word.Substring(1) + word[0] + "ay";
+        pigLatin += pigLatinWord + " ";
+    }
+    return pigLatin;
+
+}
+
+Console.WriteLine(PigLatinTranslator(new String[] { "Hola Mundo" }));
+
+//Darlyn Valerio
